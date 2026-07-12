9c73bd16c1be382a69609d6d749ffcca4619888f
joao-Devr <jpcrodrigues75@gmail.com>
2026-07-12 20:31:11 -0300

    Criando página de um tópico específico com todas as anotações

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,13 +9,15 @@ class Topic(models.Model):
 
     def __str__(self):
         """Retorna uma representação em string do modelo no painel de admin."""
-        return f"{self.text} - {self.date_added.astimezone().strftime('%d/%m/%Y %H:%M:%S')}"
+        return f"{self.text}"
     
 class Entry(models.Model):
     """Algo específico aprendido sobre um assunto."""
 
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE) 
+
     """ o on_delete=models.CASCADE significa que se o tópico for excluído, todas as entradas relacionadas a ele também serão excluídas. """
+
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -14,3 +14,15 @@ def topics(request):
     context = {'topics': topics}
 
     return render(request, 'topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único tópico e todas as suas entradas."""
+
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+
+    context = {'topic': topic,
+               'entries': entries
+              }
+
+    return render(request, 'topic.html', context)
