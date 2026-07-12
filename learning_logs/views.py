b83d781a1770548064d46b66d9ae417f34cb4c4f
joao-Devr <jpcrodrigues75@gmail.com>
2026-07-12 20:02:25 -0300

    uso do block contend e mostrando dados do BD no html topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """A página inicial do Learning Log."""
 
     return render(request, 'index.html')
+
+def topics(request):
+    """A página de tópicos do Learning Log."""
+
+    topics = Topic.objects.order_by('date_added')
+
+    context = {'topics': topics}
+
+    return render(request, 'topics.html', context)
